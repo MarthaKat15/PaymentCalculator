e39e37e9244574c0040f75bea846f05ef721164b
Martha Katsiki <n15757@newday.co.uk>
2022-02-28 10:29:34 +0000

    Initial commit

---
+++ shared/src/commonMain/kotlin/com/newday/paymentcalculator/Greeting.kt
@@ -0,0 +1,7 @@
+package com.newday.paymentcalculator
+
+class Greeting {
+    fun greeting(): String {
+        return "Hello, ${Platform().platform}!"
+    }
+}
---
+++ shared/src/commonMain/kotlin/com/newday/paymentcalculator/Platform.kt
@@ -0,0 +1,5 @@
+package com.newday.paymentcalculator
+
+expect class Platform() {
+    val platform: String
+}
